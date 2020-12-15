ae99913eceef506802e96cc1b958133e8995368d
whybfq <littlesanner@gmail.com>
2020-12-15 21:24:13 +0700

    add the 10-10 change the plot

---
+++ 10-10/Nodes2.py
@@ -0,0 +1,43 @@
+import time
+import numpy as np
+from numpy import random
+
+
+def mean_squared_error(a, b=0):
+    return 0.5 * np.sum((a - b) ** 2)  # np.square(a-b).mean()
+
+
+iteration = 1
+input = np.array([ -98.36499, -45.615704, 35.624004, -91.82865, -158.26599, -147.02539,
+                   36.7076, 20.750473, 14.900537, -171.53038, -66.60556, -99.393616,
+                   -25.43512, -66.25455, 77.57463, -212.91815, -53.07083, 137.94112,
+                   -103.54969, -214.36864 ])
+
+plaintext = np.array([  97, 98, 101, 10, 27 ])
+
+start_time = time.time()
+
+# for i in range(2, len(plaintext) + 1):
+#     W1 = (3 + 3) * np.random.random_sample((len(input), i)) - 3
+#     guess = input.dot(W1)
+#     loss = mean_squared_error(guess, plaintext[ :i ]) + mean_squared_error(plaintext[ i: ])
+# print(np.sum(guess == plaintext))
+
+min_loss = len(plaintext)
+i = 2  # the number of nodes
+
+while 1:
+    print(f"Iteration: {iteration}")
+    W1 = (4 + 4) * np.random.random_sample((len(input), i)) - 4
+    guess = input.dot(W1)
+    # loss = mean_squared_error(guess, plaintext[ :i ]) + mean_squared_error(plaintext[ i: ])
+    error = min(min_loss, ( len(plaintext) - len(np.intersect1d(guess, plaintext)) ) )
+    if error < 1:
+        break
+    else:
+        print(f"Minimum error is {error}")
+        iteration += 1
+    used_time = time.time() - start_time
+    if used_time > 3600 * 8:
+        break
+print(f"used_time is: {used_time}")
---
+++ 20-5/plot.py
@@ -0,0 +1,111 @@
+import time
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+
+Nodes = np.arange(2, 15)  # 13 nodes
+# print(Nodes)
+
+Times = np.array([ 28800.00003
+                     , 28800
+                     , 28800.00002
+                     , 28800
+                     , 28800.00002
+                     , 28800.00004
+                     , 28800.00823
+                     , 28800.00003
+                     , 28800.00001
+                     , 28800
+                     , 28800
+                     , 28800.00004
+                     , 28800.00002 ])
+
+Loss = np.array([ 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 ])
+
+Iterations = [ 482277173
+    , 477629785
+    , 433702694
+    , 424399900
+    , 425888105
+    , 428059217
+    , 1031846801
+    , 1037774429
+    , 1024399360
+    , 1033585786
+    , 1043560696
+    , 1042331850
+    , 1017113489 ]
+
+# plt.xlabel('Nodes')
+# plt.ylabel('Error')
+# plt.title('Error versus nodes')
+# plt.ylim(1, 16)
+# for a, b in zip(Nodes, Loss):
+#     plt.text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
+# plt.bar(Nodes, Loss, width=0.35, align='edge', color='c', alpha=0.8)
+# plt.show()
+#
+#
+# plt.xlabel('Nodes')
+# plt.ylabel('Crack Time (seconds)')
+# plt.title('Crack time (Decryption Time: 1.232 seconds)')
+# # plt.plot(Nodes, Times)
+# for a, b in zip(Nodes, Times):
+#     plt.text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
+# plt.bar(Nodes, Times, width=0.35, align='edge', color='c', alpha=0.8)
+# plt.plot(Nodes, [ 1.232 ] * len(Nodes), 'r', c='g', linewidth=4)
+# # plt.ylim(1.232, 30000)
+# plt.show()
+#
+#
+# plt.xlabel('Nodes')
+# plt.ylabel('Iterations')
+# plt.title('Iterations versus different output nodes')
+# plt.plot(Nodes, Iterations)
+# # for a, b in zip(Nodes, Iterations):
+# #     plt.text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
+# # plt.bar(Nodes, Iterations, width=0.15, align='center', color='c', alpha=0.8)
+#
+# plt.show()
+
+
+
+# plt
+
+
+pictures = [ 'Error versus nodes', 'Crack time (Decryption Time: 1.232 seconds)',
+             'Iterations versus nodes' ]
+fig = plt.figure(num=None, figsize=(26, 12), dpi=80, facecolor='w', edgecolor='k')
+
+fig, ax_lst = plt.subplots(1, 3, figsize=(18, 5))
+for i, str_ in enumerate(pictures):
+    ax_lst[ i ].title.set_text(str_)
+
+ax_lst[ 0 ].plot(Nodes, Loss)
+ax_lst[ 0 ].set_xlabel('nodes')
+ax_lst[ 0 ].set_ylabel('Error')
+# ax_lst[ 0 ].title('Error versus nodes')
+# plt.ylim(0, 16)
+for a, b in zip(Nodes, Loss):
+    ax_lst[ 0 ].text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
+ax_lst[ 0 ].bar(Nodes, Loss, width=0.35, align='edge', color='c', alpha=0.8)
+
+
+ax_lst[ 1 ].plot(Nodes, Times)
+ax_lst[ 1 ].set_xlabel('Nodes')
+ax_lst[ 1 ].set_ylabel('Time (seconds)')
+for a, b in zip(Nodes, Times):
+    ax_lst[ 1 ].text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
+ax_lst[ 1 ].bar(Nodes, Times, width=0.35, align='edge', color='c', alpha=0.8)
+ax_lst[ 1 ].plot(Nodes, [ 1.232 ] * len(Nodes), 'r', c='g', linewidth=4)
+# plt.ylim(1.232, 30000)
+
+ax_lst[ 2 ].plot(Nodes, Iterations)
+ax_lst[ 2 ].set_xlabel('Nodes')
+ax_lst[ 2 ].set_ylabel('Iterations')
+
+# for a, b in zip(Nodes, Iterations):
+#     ax_lst[ 2 ].text(a, b + 0.5, '%.1f' % b, ha='right', va='bottom', fontsize=7)
+# ax_lst[ 2 ].bar(Nodes, Iterations, width=0.15, align='center', color='c', alpha=0.8)
+ax_lst[ 2 ].plot(Nodes, Iterations)
+plt.show()
